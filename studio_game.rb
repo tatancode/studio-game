2dea2b87b07786f81ec0b98c40c36147391c0f2b
Seb <bustosospinat@gmail.com>
2023-10-11 15:53:21 -0500

    adding studio_game

---
+++ studio_game.rb
@@ -0,0 +1,9 @@
+puts "Let's play a game!\n\n 3 \n 2 \n 1"
+welcome_emoji = "🚀"
+puts welcome_emoji * 20
+
+# Player data
+name = "Finn"
+health = 60
+health_status = "#{name}'s health is #{health / 3.0}"
+puts health_status
