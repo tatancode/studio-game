572e997fa54331bf3d682213735ebc9a10c5e0c3
Seb <bustosospinat@gmail.com>
2023-10-17 16:14:48 -0500

    addding methods

--- studio_game.rb
+++ studio_game.rb
@@ -1,9 +1,17 @@
+#Welcome message
 puts "Let's play a game!\n\n 3 \n 2 \n 1"
 welcome_emoji = "🚀"
 puts welcome_emoji * 20
 
 # Player data
-name = "Finn"
+name = "finn"
 health = 60
-health_status = "#{name}'s health is #{health / 3.0}"
-puts health_status
+
+
+# helper method that prints Health Info -uses keyword parameters-
+def health_info(name:,health:)
+  health_status = "#{name.capitalize}'s health is #{health}"
+  puts health_status
+end
+
+health_info(health: health ,name: name )
