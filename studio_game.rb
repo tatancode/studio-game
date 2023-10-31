f01c30248d792be231bc274100c15b42767afbd1
Seb <bustosospinat@gmail.com>
2023-10-31 16:17:50 -0500

    adding class Player

--- studio_game.rb
+++ studio_game.rb
@@ -4,16 +4,20 @@ class Player
   #attributes
   #attr_reader :name, :health
   attr_accessor :name, :health
+
+  #initialize is Ruby's constructor
   def initialize(name,health = 100)
     @name = name.capitalize
     @health = health
   end
 
 
+  # this method is executed when the puts method is run on a Player object
   def to_s
     "I'm #{@name.capitalize} with a health of #{@health} and a score of #{score}"
   end
 
+  # helper methods
   def score
     @health + @name.length
   end
@@ -25,13 +29,30 @@ class Player
   def drain
     @health -= 1
   end
+
+  # method that changes a player's health based on the roll of a die
+  def play(player)
+    # if low number 1 or 2, drain player health
+    number_rolled = rand(1..6)
+    if number_rolled < 3
+      player.drain
+      puts "#{player.name} got drained :("
+    # if medium number 3 or 4, skip the player - no changes
+    elsif number_rolled > 2 && number_rolled < 5
+      puts "#{player.name} got skipped lol"
+    # if high number 5 or 6, boost the player
+    else
+      player.boost
+      puts "#{player.name} got boosted :)"
+    end
+  end
 end
 
 #initializing objects
-player_one = Player.new("Finn", 10)
-player_one.score
+player_one = Player.new("Finn", 60)
 puts player_one
-
+player_one.play(player_one)
+puts player_one
 
 =begin
 player_two = Player.new("Phoebs", 10)
